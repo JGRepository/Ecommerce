9fd63871e0e70186ecf91812a06f9c92b1b19677
GuilhermeSilva <biojgs@gmail.com>
2023-07-29 19:52:12 -0300

    feat: adicionando elementos a pagina de contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import  {ContacUsComponent} from "./contac-us/contac-us.component"
+import { VitrineComponent } from './vitrine/vitrine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/vitrine', pathMatch: 'full'},
+  {path: 'vitrine', component:VitrineComponent},
+  {path: 'contact', component:ContacUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/contac-us/contac-us.component.ts
+++ src/app/contac-us/contac-us.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contac-us',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contac-us.component.scss']
 })
 export class ContacUsComponent {
+  @ViewChild('inputName') nameInputRef!: ElementRef;
+  @ViewChild('inputEmail') emailInputRef!: ElementRef;
+  @ViewChild('textArea') textArealInputRef!: ElementRef;
 
+  OnSubmit() {
+
+    const nameInp = this.nameInputRef.nativeElement.value;
+    const emailInp = this.emailInputRef.nativeElement.value;
+    const textArea = this.textArealInputRef.nativeElement.value;
+  }
+  OnClose() {
+
+    this.nameInputRef.nativeElement.value = '';
+    this.emailInputRef.nativeElement.value = '';
+    this.textArealInputRef.nativeElement.value = '';
+  }
 }
+
