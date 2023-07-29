b91b2463899cb36c9b2254e51ea805c5302416b0
GuilhermeSilva <biojgs@gmail.com>
2023-07-29 14:51:23 -0300

    feat: criando as primeiras estruturas de páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContacUsComponent } from './contac-us/contac-us.component';
+import { HeaderComponent } from './header/header.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContacUsComponent,
+    HeaderComponent,
+    VitrineComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contac-us/contac-us.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContacUsComponent } from './contac-us.component';
+
+describe('ContacUsComponent', () => {
+  let component: ContacUsComponent;
+  let fixture: ComponentFixture<ContacUsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ContacUsComponent]
+    });
+    fixture = TestBed.createComponent(ContacUsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
